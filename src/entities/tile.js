5fad49a0328b34276146ff341e3e2649531e35b3
Daniel Whiffing <danielwhiffing@gmail.com>
2016-03-05 10:03:52 -0500

    Lays down some random tiles

--- src/entities/tile.js
+++ src/entities/tile.js
@@ -1,29 +1,11 @@
 export default class Tile extends Phaser.Sprite {
-
-  constructor(game, x, y, size, group) {
-    super(game, x, y, 'tile')
-    this.game = game
-    game.physics.enable(this)
-    this.body.setSize(size*1.3,size*1.3)
-    this.frame= game.rnd.between(0,9)
-    this.width = size
-    this.height = size
-    this.anchor.setTo(0.5,0.5)
-    group.add(this)
-  }
-
-  update() {
-  }
-
-  spawn() {
-    this.alive = true
-    this.alpha = 1
-    this.frame = 0
-  }
-
-  destry() {
-    this.frame = 9
-    this.alpha = 0
-    this.alive = false
+  constructor(game, x, y, frame=0, rotation=0, scale=0.1) {
+    super(game, x, y, 'tiles')
+    this.anchor.setTo(0.5)
+    this.scale.setTo(scale)
+    this.x+=this.width/2
+    this.y+=this.height/2
+    this.frame = frame
+    this.angle = rotation * 90
   }
 }
--- src/states/load.js
+++ src/states/load.js
@@ -2,12 +2,7 @@ export default {
   preload() {
     this.load.onLoadComplete.addOnce(this.onLoadComplete, this)
 
-    this.game.load.spritesheet('explosion', 'images/explosion.png', 128, 128);
-    this.load.spritesheet('bar', 'images/bar.png', 150, 20);
-    this.load.image('cross', 'images/Line.png');
-    this.load.image('displacement', 'images/displacement_map.png');
-    this.load.spritesheet('tile', 'images/button.png', 300, 300);
-    this.load.script('filter', 'DisplacementFilter.js');
+    this.load.spritesheet('tiles', 'images/tiles.png', 200, 200);
   },
 
   onLoadComplete() {
