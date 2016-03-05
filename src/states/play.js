4a4de02fa26fa04b290a96abe9320f11237a931b
Daniel Whiffing <danielwhiffing@gmail.com>
2016-03-05 18:01:09 -0500

    cleanup

--- src/states/play.js
+++ src/states/play.js
@@ -4,32 +4,19 @@ import LightManager from '../entities/lightManager'
 import Player from '../entities/player'
 import InputManager from '../entities/InputManager'
 
-let shadowTexture, line
-
 export default {
   create(game) {
+    game.physics.startSystem(Phaser.Physics.ARCADE)
+
     this.level = new Level(game, 9)
     this.miniMap = new MiniMap(game, this.level)
 
-    // game.physics.startSystem(Phaser.Physics.ARCADE)
     game.world.setBounds(0, 0, this.level.group.width, this.level.group.height)
-
-    // game.canvas.addEventListener('mousedown', () => game.input.mouse.requestPointerLock())
-
     game.camera.x = game.world.width/2-game.canvas.width/2
     game.camera.y = game.world.height/2-game.canvas.height/2
 
-    // game.input.addMoveCallback((pointer, x, y) => {
-    //   if (game.input.mouse.locked) {
-    //     game.camera.x += game.input.mouse.event.webkitMovementX
-    //     game.camera.y += game.input.mouse.event.webkitMovementY
-    //   }
-    // }, this)
-
     this.player = new Player(game)
-
     this.lightManager = new LightManager(game, 100)
-
     game.world.bringToTop(this.miniMap.group)
 
     this.inputManager = new InputManager(game)
@@ -41,9 +28,6 @@ export default {
     //   this.level.map.rejigger()
     //   this.miniMap.update()
     // })
-    this.inputManager.bind("w", () => {
-      this.miniMap.update()
-    })
   },
 
   update(game) {
